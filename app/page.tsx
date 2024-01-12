b2a8f2b11a014cca44bbb65f70c390e291f6c111
rosyid <rajamasker@gmail.com>
2024-01-12 17:56:09 +0900

    change font

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className="flex min-h-screen w-screen p-24 place-items-center justify-center">
+      <div className="bg-white p-1 h-fit gap-1 w-96 flex place-items-center rounded">
+        <div className="bg-slate-300 rounded-full size-12"></div>
+        <div className="text-wrap text-red-500">
+          <h1>Sample</h1>
+        </div>
+      </div>
     </main>
   )
 }
